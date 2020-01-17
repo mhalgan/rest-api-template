9c481581de833e857295807971fc933bd8392289
Glauber Camargo Campos Rocha <gccr92@gmail.com>
2020-01-17 15:51:37 -0300

    Added intial swagger documentation

---
+++ src/config/swaggerDef.js
@@ -0,0 +1,9 @@
+module.exports = {
+  info: {
+    title: 'REST API template',
+    version: '1.0.0',
+    description: 'Full functional scalable REST API Template'
+  },
+  basePath: '/api',
+  apis: ['src/controllers/apis/v1/*.js']
+}
--- src/routes/apis/index.js
+++ src/routes/apis/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
-const v1ApiController = require('./v1')
 let router = express.Router()
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../../../swagger.json')
+const v1ApiController = require('./v1')
+
+router.use('/', swaggerUi.serve)
+router.get('/', swaggerUi.setup(swaggerDocument))
 
 router.use('/v1', v1ApiController)
 module.exports = router
