2dd7f8425bb26b4f71b0ce4807d0b39bd097b8ee
Glauber Camargo Campos Rocha <gccr92@gmail.com>
2020-01-21 09:39:35 -0300

    Fixed authGuard erro handling

--- src/middlewares/authGuard.js
+++ src/middlewares/authGuard.js
@@ -6,17 +6,15 @@ const authClientToken = async (req, res, next) => {
   let token = req.headers['authorization']
 
   if (!token) {
-    return next(
-      res.status(401).json({ errors: [{ msg: 'No token provided' }] })
-    )
+    res.status(401)
+    return next('no token provided')
   }
 
   // Check if token is valid
   jwt.verify(token, process.env.PRIVATE_KEY, (err, decoded) => {
     if (err) {
-      return next(
-        res.status(401).json({ errors: [{ msg: 'Invalid token', error: err }] })
-      )
+      res.status(401)
+      return next({ errors: [{ msg: 'invalid token' }], realError: err })
     }
     next()
   })
