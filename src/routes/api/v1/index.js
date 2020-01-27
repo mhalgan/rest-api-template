08e9676cc259275d21074e887dbed07ed2eef8c3
Glauber Camargo Campos Rocha <gccr92@gmail.com>
2020-01-27 09:56:28 -0300

    removed todos reference

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,6 +1,5 @@
 const authClientRequest = require('../../../middlewares/authGuard')
 const userRoutes = require('./users.route')
-const todosRoutes = require('./todos.route')
 const authRoutes = require('./auth.route')
 
 const express = require('express')
@@ -9,6 +8,5 @@ let router = express.Router()
 // Add routes
 router.use('/auth', authRoutes)
 router.use('/users', authClientRequest.authClientToken, userRoutes)
-router.use('/todos', authClientRequest.authClientToken, todosRoutes)
 
 module.exports = router
